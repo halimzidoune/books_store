846e344c4d01d9b51f6aae7d4ef88b20097577e9
halimzidoune@gmail.com <halimzidoune@gmail.com>
2024-01-15 16:30:05 +0100

    first commit

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreBookRequest;
+use App\Http\Requests\UpdateBookRequest;
+use App\Models\Book;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Book::query()->orderBy('id', 'DESC')->paginate(10);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreBookRequest $request)
+    {
+        $book = Book::create($request->validated());
+
+        return response()->json($book, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Book $book)
+    {
+        return $book;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateBookRequest $request, Book $book)
+    {
+        $book->update($request->validated());
+
+        return response()->json($book, 201);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Book $book)
+    {
+        $book->delete();
+        return response()->noContent();
+    }
+}
